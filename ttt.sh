e02ffbb8137fd6f577b60e563f4e0a5696848a6b
Anil_Soni007 <115683735+anilsoni007@users.noreply.github.com>
2026-06-15 17:41:08 +0530

    Create ttt.sh

---
+++ ttt.sh
@@ -0,0 +1,29 @@
+import json
+import urllib3
+import os
+
+http = urllib3.PoolManager()
+
+def lambda_handler(event, context):
+    jenkins_url = os.environ['JENKINS_URL']
+    jenkins_token = os.environ['JENKINS_TOKEN']
+    
+    # Forward GitHub webhook to Jenkins
+    headers = {
+        'Content-Type': 'application/json',
+        'X-GitHub-Event': event['headers'].get('X-GitHub-Event', ''),
+        'X-GitHub-Delivery': event['headers'].get('X-GitHub-Delivery', ''),
+        'Authorization': f'Bearer {jenkins_token}'
+    }
+    
+    response = http.request(
+        'POST',
+        f'{jenkins_url}/github-webhook/',
+        body=event['body'],
+        headers=headers
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Webhook forwarded'})
+    }
